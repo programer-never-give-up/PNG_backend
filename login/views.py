be9ad3844eae5b26acced2fead0c07691de03b2e
Inderway <1040214708@qq.com>
2019-08-24 00:41:52 +0800

    可使用session
    session在登录期间永久存在

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from . import models
+from . import forms
 # Create your views here.
 
 def index(request):
@@ -10,7 +11,10 @@ def index(request):
 
 
 def login(request):
+    if request.session.get('is_login',None):#不允许重复登录
+        return redirect('/index/')
     if request.method == 'POST':
+        #login_form = forms.UserForm(request.POST)
         username = request.POST.get('username')
         password = request.POST.get('password')
         message = '请检查填写的内容！'
@@ -18,20 +22,33 @@ def login(request):
             # 用户名字符合法性验证
             # 密码长度验证
             # 更多的其它验证.....
+        # #if login_form.is_valid():
+        #     username = login_form.cleaned_data.get('username')#获取表单值
+        #     password = login_form.cleaned_data.get('password')
+
+
             try:
                 user = models.User.objects.get(name=username)
             except :
                 message = '用户不存在！'
-                return render(request, 'login/login.html', {'message': message})
+                return render(request, 'login/login.html', {'message':message})#返回当前所有的本地变量字典
 
             if user.password == password:
+                #在session字典加入用户状态
+
+                request.session['is_login']=True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
+                request.session.set_expiry(10)#十秒过期，无效
                 print(username, password)
                 return redirect('/index/')
             else:
                 message = '密码不正确！'
-                return render(request, 'login/login.html', {'message': message})
+                return render(request, 'login/login.html',{'message':message})
         else:
-            return render(request, 'login/login.html', {'message': message})
+            return render(request, 'login/login.html', {'message':message})#若验证不通过
+
+    #login_form=forms.UserForm    #空表单
     return render(request, 'login/login.html')
 
 
@@ -41,7 +58,14 @@ def register(request):
 
 
 def logout(request):
-    pass
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/login/")
+    request.session.flush()#删除会话
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
     return redirect("/login/")
 
 def verify(request):
